5ce8815668a807dc64ea34468bfc61b43f4e28af
Gobzi <jnlazaridis@gmail.com>
2022-11-21 15:30:58 +0000

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -4,21 +4,20 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-proxies = {"http": "http://127.0.0.1:8080", "https": "http://127.0.0.1:8080"}
-methods = ['get', 'put','post','head','patch','delete']
 
 
 parser = argparse.ArgumentParser(description='Checks all Methods for a single API endpoint')
 parser.add_argument('--url',  type=str, required=True, help='Enter the url')
+parser.add_argument('--methods',  nargs='+',  type=str, required=True, help='Enter the url')
+parser.add_argument('--proxy',  type=str, required=False, help='Enter IP:Port')
 parser.add_argument('--header',  type=str, required=False, help='Enter headers')
+
+
 args = parser.parse_args()
-
-
-
 endpoint=(args.url)
-print (args.header)
+methods=(args.methods)
+proxies = {"http": args.proxy, "https": args.proxy}
 headers = {args.header.split(':')[0]:args.header.split(':')[1].replace(" ", "")}
-print (headers)
 
 for i in methods:
     req=requests.request(i, endpoint, headers=headers, proxies=proxies, verify=False)
