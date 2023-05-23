87792c24d519945defe3d57a8af6c9a61ea9ce05
Gobzi <jnlazaridis@gmail.com>
2023-05-23 12:03:32 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -11,18 +11,29 @@ parser.add_argument('--url',  type=str, required=True, help='Enter the url')
 parser.add_argument('--methods',  nargs='+',  type=str, required=True, help='Enter the url')
 parser.add_argument('--proxy',  type=str, required=False, help='Enter IP:Port')
 parser.add_argument('--header',  type=str, required=False, help='Enter headers')
+parser.add_argument('--wordlist',  type=str, required=True, help='Enter path for Wordlist')
 
 
 args = parser.parse_args()
 endpoint=(args.url)
 methods=(args.methods)
 proxies = {"http": args.proxy, "https": args.proxy}
-headers = {args.header.split(':')[0]:args.header.split(':')[1].replace(" ", "")}
+f = open(args.wordlist, "r")
+
+if args.header:
+    headers = {args.header.split(':')[0]:args.header.split(':')[1].replace(" ", "")}
+else:
+    headers = {}
+
+if not endpoint.startswith("http://") and not endpoint.startswith("https://"):
+    print("The URL must start with http:// or https://. Please try again.")
+    exit()
 
 for i in methods:
-    req=requests.request(i, endpoint, headers=headers, proxies=proxies, verify=False)
-    code =req.status_code
-    text = req.text
-    rsp = '"error":"Internal Server Error"'
-    print (i.upper(),"-",code)
-    print (text)
+    for line in f:
+        endpoint = endpoint + "/" + line.strip()
+        req=requests.request(i, endpoint, headers=headers, proxies=proxies, verify=False)
+        code =req.status_code
+        text = req.text
+        rsp = '"error":"Internal Server Error"'
+        print (i.upper(),"-",code)
